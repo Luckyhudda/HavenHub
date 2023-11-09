ed2969417c86ed2cb54a4c13e700591fbfa58057
Luckyhudda <mr.luckyhudda@gmail.com>
2023-11-09 14:15:05 +0530

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
+require('dotenv').config();
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connect to MongoDB");
+  })
+  .catch(() => {
+    console.log("error in DB connection");
+  });
+
+
+
+
+
+
 
 app.listen(3000, () =>{
     console.log('server  Started...')
