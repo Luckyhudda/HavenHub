4b1f16d8bc38e7184867b272d805b71078e63802
Luckyhudda <mr.luckyhudda@gmail.com>
2023-11-09 16:21:02 +0530

    add cors module and

--- api/controlers/authController.js
+++ api/controlers/authController.js
@@ -1,17 +1,17 @@
 const  User  = require("../model/userModel");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
 const catchError = require("../utlis/error");
 
-const authControler =  {
+const authControler = {
   signup: async (req, res,next) => {
     const { username, email, password } = req.body;
 
     const user = await User.find({email});
 
-    if (user.length > 0) {
-      return next(catchError(402, "User already exists"));
-    }
+   if (user.length > 0) {
+     return next(catchError(409, "User already exists"));
+   }
     const hashedPassword = bcrypt.hashSync(password, 12);
     const newUser = new User({ username, email, password: hashedPassword });
     try {
--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const app = express();
+app.use(express.json())
+// app.use(cors());
 const authRouter = require('./routes/authRoutes')
 
 mongoose
@@ -16,7 +19,7 @@ mongoose
   // ROUTES....
   app.use("/api/auth", authRouter);
 
-app.listen(3000, () => {
+app.listen(2001, () => {
   console.log("server  Started...");
 });
 
