f6c260fe9131bbfa6156aee0164db879db338101
Luckyhudda <mr.luckyhudda@gmail.com>
2023-11-09 14:02:35 +0530

    route And pages

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/Signin"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import Header from "./components/Header"
+
+function App() {
+ 
+  return (
+    <>
+      <Header/>
+     <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/profile" element={<Profile/>} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sing-up" element={<SignUp/>} />
+     </Routes>
+    </>
+  )
+}
+
+export default App
